d859aedb477878e33eb26c090f14f3cd9a79d5e8
Franc Tauste González <franctauste2000@gmail.com>
2025-03-25 18:07:17 +0100

    Descripción de los usuario añadida en la página de usuarios registrados

--- src/main/java/todolist/controller/HomeController.java
+++ src/main/java/todolist/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import todolist.authentication.ManagerUserSession;
 import todolist.dto.UsuarioData;
 import todolist.service.UsuarioService;
@@ -35,5 +36,11 @@ public class HomeController {
     public String registered(Model model) {
         model.addAttribute("users",usuarioService.getAllUsers());
         return "registered";
+    }
+
+    @GetMapping("/registered/{id}")
+    public String registeredUserDetails(@PathVariable(value="id") Long idUsuario,Model model) {
+        model.addAttribute("user",usuarioService.findById(idUsuario));
+        return "registeredUserDetails";
     }
 }
